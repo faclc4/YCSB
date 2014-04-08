ac869ce819997126a4581bc6dd3c0ebf9d199d72
otrack <0track@gmail.com>
2014-04-08 19:54:26 +0200

    [FIX] indentation

--- src/main/java/com/yahoo/ycsb/Client.java
+++ src/main/java/com/yahoo/ycsb/Client.java
@@ -142,276 +142,278 @@ class StatusThread extends Thread
  */
 class ClientThread extends Thread
 {
-	DB _db;
-	boolean _dotransactions;
-        boolean _replay;
-        boolean _speedup;
-	Workload _workload;
-	int _opcount;
-	double _target;
+    DB _db;
+    boolean _dotransactions;
+    boolean _replay;
+    boolean _speedup;
+    Workload _workload;
+    int _opcount;
+    double _target;
 
-	int _opsdone;
-	int _threadid;
-	int _threadcount;
-	Object _workloadstate;
-	Properties _props;
+    int _opsdone;
+    int _threadid;
+    int _threadcount;
+    Object _workloadstate;
+    Properties _props;
 
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param db the DB implementation to use
-	 * @param dotransactions true to do transactions, false to insert data
-	 * @param workload the workload to use
-	 * @param threadid the id of this thread 
-	 * @param threadcount the total number of threads 
-	 * @param props the properties defining the experiment
-	 * @param opcount the number of operations (transactions or inserts) to do
-	 * @param targetperthreadperms target number of operations per thread per ms
-	 */
-	public ClientThread(DB db, boolean replay, boolean dotransactions, Workload workload, int threadid, int threadcount, Properties props, int opcount, double targetperthreadperms)
-	{
-		//TODO: consider removing threadcount and threadid
-		_db=db;
-		_dotransactions=dotransactions;
-                _replay = replay;
-		_workload=workload;
-		_opcount=opcount;
-		_opsdone=0;
-		_target=targetperthreadperms;
-		_threadid=threadid;
-		_threadcount=threadcount;
-		_props=props;
-		//System.out.println("Interval = "+interval);
-	}
+    /**
+     * Constructor.
+     *
+     * @param db the DB implementation to use
+     * @param dotransactions true to do transactions, false to insert data
+     * @param workload the workload to use
+     * @param threadid the id of this thread
+     * @param threadcount the total number of threads
+     * @param props the properties defining the experiment
+     * @param opcount the number of operations (transactions or inserts) to do
+     * @param targetperthreadperms target number of operations per thread per ms
+     */
+    public ClientThread(DB db, boolean replay, boolean dotransactions, Workload workload, int threadid, int threadcount, Properties props, int opcount, double targetperthreadperms)
+    {
+        //TODO: consider removing threadcount and threadid
+        _db=db;
+        _dotransactions=dotransactions;
+        _replay = replay;
+        _workload=workload;
+        _opcount=opcount;
+        _opsdone=0;
+        _target=targetperthreadperms;
+        _threadid=threadid;
+        _threadcount=threadcount;
+        _props=props;
+        //System.out.println("Interval = "+interval);
+    }
 
-	public int getOpsDone()
-	{
-		return _opsdone;
-	}
+    public int getOpsDone()
+    {
+        return _opsdone;
+    }
 
-	public void run()
-	{
-		try
-		{
-			_db.init();
-		}
-		catch (DBException e)
-		{
-			e.printStackTrace();
-			e.printStackTrace(System.out);
-			return;
-		}
+    public void run()
+    {
+        try
+        {
+            _db.init();
+        }
+        catch (DBException e)
+        {
+            e.printStackTrace();
+            e.printStackTrace(System.out);
+            return;
+        }
 
-		try
-		{
-			_workloadstate=_workload.initThread(_props,_threadid,_threadcount);
-		}
-		catch (WorkloadException e)
-		{
-			e.printStackTrace();
-			e.printStackTrace(System.out);
-			return;
-		}
+        try
+        {
+            _workloadstate=_workload.initThread(_props,_threadid,_threadcount);
+        }
+        catch (WorkloadException e)
+        {
+            e.printStackTrace();
+            e.printStackTrace(System.out);
+            return;
+        }
 
-		//spread the thread operations out so they don't all hit the DB at the same time
-		try
-		{
-		   //GH issue 4 - throws exception if _target>1 because random.nextInt argument must be >0
-		   //and the sleep() doesn't make sense for granularities < 1 ms anyway
-		   if ( (_target>0) && (_target<=1.0) ) 
-		   {
-		      sleep(Utils.random().nextInt((int)(1.0/_target)));
-		   }
-		}
-		catch (InterruptedException e)
-		{
-		  // do nothing.
-		}
-		
-		try
-		{
-                    if(!_replay){
+        //spread the thread operations out so they don't all hit the DB at the same time
+        try
+        {
+            //GH issue 4 - throws exception if _target>1 because random.nextInt argument must be >0
+            //and the sleep() doesn't make sense for granularities < 1 ms anyway
+            if ( (_target>0) && (_target<=1.0) )
+            {
+                sleep(Utils.random().nextInt((int)(1.0/_target)));
+            }
+        }
+        catch (InterruptedException e)
+        {
+            // do nothing.
+        }
 
-			if (_dotransactions)
-			{
-				long st=System.currentTimeMillis();
+        try
+        {
+            if(!_replay){
 
-				while (((_opcount == 0) || (_opsdone < _opcount)) && !_workload.isStopRequested())
-				{
+                if (_dotransactions)
+                {
+                    long st=System.currentTimeMillis();
 
-					if (!_workload.doTransaction(_db,_workloadstate))
-					{
-						break;
-					}
+                    while (((_opcount == 0) || (_opsdone < _opcount)) && !_workload.isStopRequested())
+                    {
 
-					_opsdone++;
+                        if (!_workload.doTransaction(_db,_workloadstate))
+                        {
+                            break;
+                        }
 
-					//throttle the operations
-					if (_target>0)
-					{
-						//this is more accurate than other throttling approaches we have tried,
-						//like sleeping for (1/target throughput)-operation latency,
-						//because it smooths timing inaccuracies (from sleep() taking an int, 
-						//current time in millis) over many operations
-						while (System.currentTimeMillis()-st<((double)_opsdone)/_target)
-						{
-							try
-							{
-								sleep(1);
-							}
-							catch (InterruptedException e)
-							{
-							  // do nothing.
-							}
+                        _opsdone++;
 
-						}
-					}
-				}
-			}
-			else
-			{
-				long st=System.currentTimeMillis();
+                        //throttle the operations
+                        if (_target>0)
+                        {
+                            //this is more accurate than other throttling approaches we have tried,
+                            //like sleeping for (1/target throughput)-operation latency,
+                            //because it smooths timing inaccuracies (from sleep() taking an int,
+                            //current time in millis) over many operations
+                            while (System.currentTimeMillis()-st<((double)_opsdone)/_target)
+                            {
+                                try
+                                {
+                                    sleep(1);
+                                }
+                                catch (InterruptedException e)
+                                {
+                                    // do nothing.
+                                }
 
-				while (((_opcount == 0) || (_opsdone < _opcount)) && !_workload.isStopRequested())
-				{
-
-					if (!_workload.doInsert(_db,_workloadstate))
-					{
-						break;
-					}
-
-					_opsdone++;
-
-					//throttle the operations
-					if (_target>0)
-					{
-						//this is more accurate than other throttling approaches we have tried,
-						//like sleeping for (1/target throughput)-operation latency,
-						//because it smooths timing inaccuracies (from sleep() taking an int, 
-						//current time in millis) over many operations
-						while (System.currentTimeMillis()-st<((double)_opsdone)/_target)
-						{
-							try 
-							{
-								sleep(1);
-							}
-							catch (InterruptedException e)
-							{
-							  // do nothing.
-							}
-						}
-					}
-				}
-			}
+                            }
+                        }
                     }
-                    //#######################################################################################
-                    //#######################################################################################
-                    //
-                    //if its the replay operation mode:
-                    //
-                    //#######################################################################################
-                    //#######################################################################################
-                    else{
-                        if (_dotransactions)
-			{
-				long st=System.currentTimeMillis();
+                }
+                else
+                {
+                    long st=System.currentTimeMillis();
 
-				while (((_opcount == 0) || (_opsdone < _opcount)) && !_workload.isStopRequested())
-				{
+                    while (((_opcount == 0) || (_opsdone < _opcount)) && !_workload.isStopRequested())
+                    {
 
-					if (!_workload.doTransactionReplay(_db,_workloadstate))
-					{
-						break;
-					}
+                        if (!_workload.doInsert(_db,_workloadstate))
+                        {
+                            break;
+                        }
 
-					_opsdone++;
+                        _opsdone++;
 
-					//throttle the operations
-					if (_target>0)
-					{
-						//this is more accurate than other throttling approaches we have tried,
-						//like sleeping for (1/target throughput)-operation latency,
-						//because it smooths timing inaccuracies (from sleep() taking an int, 
-						//current time in millis) over many operations
-						while (System.currentTimeMillis()-st<((double)_opsdone)/_target)
-						{
-							try
-							{
-								sleep(1);
-							}
-							catch (InterruptedException e)
-							{
-							  // do nothing.
-							}
+                        //throttle the operations
+                        if (_target>0)
+                        {
+                            //this is more accurate than other throttling approaches we have tried,
+                            //like sleeping for (1/target throughput)-operation latency,
+                            //because it smooths timing inaccuracies (from sleep() taking an int,
+                            //current time in millis) over many operations
+                            while (System.currentTimeMillis()-st<((double)_opsdone)/_target)
+                            {
+                                try
+                                {
+                                    sleep(1);
+                                }
+                                catch (InterruptedException e)
+                                {
+                                    // do nothing.
+                                }
+                            }
+                        }
+                    }
+                }
 
-						}
-					}
-				}
-			}
-			else
-			{
-				long st=System.currentTimeMillis();
+            }
 
-				while (((_opcount == 0) || (_opsdone < _opcount)) && !_workload.isStopRequested())
-				{
-                                        //ler lista de chaves por timestamp;
-                                        //calcular diferença entre timestamps --> diff
-                                        //efetuar a operacao
-                                        //esperar diff --> sleep(diff)
-                                    
-                                    
-					if (!_workload.doReplayInsert(_db,_workloadstate))
-					{
-						break;
-					}
+            //#######################################################################################
+            //#######################################################################################
+            //
+            //if its the replay operation mode:
+            //
+            //#######################################################################################
+            //#######################################################################################
+            else{
 
-					_opsdone++;
+                if (_dotransactions)
+                {
+                    long st=System.currentTimeMillis();
 
-					//throttle the operations
-					if (_target>0)
-					{
-						//this is more accurate than other throttling approaches we have tried,
-						//like sleeping for (1/target throughput)-operation latency,
-						//because it smooths timing inaccuracies (from sleep() taking an int, 
-						//current time in millis) over many operations
-						while (System.currentTimeMillis()-st<((double)_opsdone)/_target)
-						{
-							try 
-							{
-								sleep(1);
-							}
-							catch (InterruptedException e)
-							{
-							  // do nothing.
-							}
-						}
-					}
-				}
-			}
-                        
-                        
-                        
-                    }       
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-			e.printStackTrace(System.out);
-			System.exit(0);
-		}
+                    while (((_opcount == 0) || (_opsdone < _opcount)) && !_workload.isStopRequested())
+                    {
 
-		try
-		{
-			_db.cleanup();
-		}
-		catch (DBException e)
-		{
-			e.printStackTrace();
-			e.printStackTrace(System.out);
-			return;
-		}
-	}
+                        if (!_workload.doTransactionReplay(_db, _workloadstate))
+                        {
+                            break;
+                        }
+
+                        _opsdone++;
+
+                        //throttle the operations
+                        if (_target>0)
+                        {
+                            //this is more accurate than other throttling approaches we have tried,
+                            //like sleeping for (1/target throughput)-operation latency,
+                            //because it smooths timing inaccuracies (from sleep() taking an int,
+                            //current time in millis) over many operations
+                            while (System.currentTimeMillis()-st<((double)_opsdone)/_target)
+                            {
+                                try
+                                {
+                                    sleep(1);
+                                }
+                                catch (InterruptedException e)
+                                {
+                                    // do nothing.
+                                }
+
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    long st=System.currentTimeMillis();
+
+                    while (((_opcount == 0) || (_opsdone < _opcount)) && !_workload.isStopRequested())
+                    {
+                        //ler lista de chaves por timestamp;
+                        //calcular diferença entre timestamps --> diff
+                        //efetuar a operacao
+                        //esperar diff --> sleep(diff)
+
+
+                        if (!_workload.doReplayInsert(_db, _workloadstate))
+                        {
+                            break;
+                        }
+
+                        _opsdone++;
+
+                        //throttle the operations
+                        if (_target>0)
+                        {
+                            //this is more accurate than other throttling approaches we have tried,
+                            //like sleeping for (1/target throughput)-operation latency,
+                            //because it smooths timing inaccuracies (from sleep() taking an int,
+                            //current time in millis) over many operations
+                            while (System.currentTimeMillis()-st<((double)_opsdone)/_target)
+                            {
+                                try
+                                {
+                                    sleep(1);
+                                }
+                                catch (InterruptedException e)
+                                {
+                                    // do nothing.
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            e.printStackTrace(System.out);
+            System.exit(0);
+        }
+
+        try
+        {
+            _db.cleanup();
+        }
+        catch (DBException e)
+        {
+            e.printStackTrace();
+            e.printStackTrace(System.out);
+            return;
+        }
+    }
 }
 
 /**
