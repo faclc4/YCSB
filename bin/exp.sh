90e9a8f96cd88197bfffccf7ee69125aaa71e00b
otrack <0track@gmail.com>
2014-04-14 14:11:55 +0200

    [FIX] cost of storage now takes into account the workload execution

--- bin/exp.sh
+++ bin/exp.sh
@@ -61,7 +61,7 @@ do
 	wait
 	sleep 20
 
-    	echo "3. Computing storage usage (before)"
+    	echo "3. Computing storage size (before)"
     	rm -f tmp3
     	let e=${#servers[@]}-1
     	for i in `seq 0 $e`
@@ -85,20 +85,8 @@ do
     	grep RunTime tmp4    
     	sleep 20
 	
-    	echo "5. Computing storage usage (after)"
-    	rm -f tmp5
-    	let e=${#servers[@]}-1
-    	for i in `seq 0 $e`
-    	do
-    	    ${SSHCMDNODE} ${servers[$i]} "ps -ef|grep java | grep -v grep | tr -s \" \" | cut -d \" \" -f 2 | awk '{print \"/opt/oracle-jdk-bin-1.7.0.45/bin/jcmd \"\$0\" GC.class_histogram\" }' | sh 2>&1 | egrep ^Total | tr -s \" \"| cut -d \" \" -f 3" >> tmp5 &
-    	done
-    	wait
-    	avrgAfterLoad=`${lua} stats.lua tmp5 | cut -f 1`
-    	stdAfterLoad=`${lua} stats.lua tmp5 | tr -s " " | cut -f 2`    
-    	echo ${avrgAfterLoad} ${stdgAfterLoad}
-
-        echo "6. Running experiments on ${wiki} Wiki"
-        rm -f tmp7-*
+        echo "5. Running experiments on ${wiki} Wiki"
+        rm -f tmp5-*
     	for thread in ${threads}
     	do
     	    echo "Using  ${thread} threads"
@@ -106,17 +94,29 @@ do
     	    let e=${#clients[@]}-1
     	    for i in `seq 0 $e`
     	    do
-    		${SSHCMDNODE} ubuntu@${clients[$i]} ${CMD} &> tmp7-${thread} &
+    		${SSHCMDNODE} ubuntu@${clients[$i]} ${CMD} &> tmp5-${thread} &
     	    done
     	    wait
-    	    grep -i "AverageLatency(ms)" tmp7-${thread}
-    	    grep -i "Throughput" tmp7-${thread}
+    	    grep -i "AverageLatency(ms)" tmp5-${thread}
+    	    grep -i "Throughput" tmp5-${thread}
     	done
 
-    	echo "7. Computing overhead "
+    	echo "6. Computing storage size (after)"
+    	rm -f tmp6
+    	let e=${#servers[@]}-1
+    	for i in `seq 0 $e`
+    	do
+    	    ${SSHCMDNODE} ${servers[$i]} "ps -ef|grep java | grep -v grep | tr -s \" \" | cut -d \" \" -f 2 | awk '{print \"/opt/oracle-jdk-bin-1.7.0.45/bin/jcmd \"\$0\" GC.class_histogram\" }' | sh 2>&1 | egrep ^Total | tr -s \" \"| cut -d \" \" -f 3" >> tmp6 &
+    	done
+    	wait
+    	avrgAfterLoad=`${lua} stats.lua tmp6 | cut -f 1`
+    	stdAfterLoad=`${lua} stats.lua tmp6 | tr -s " " | cut -f 2`    
+    	echo ${avrgAfterLoad} ${stdgAfterLoad}
+
+    	echo "7. Storage cost"
     	avrgOverhead=`echo "(${avrgAfterLoad} - ${avrgBeforeLoad})/(10^6)" | bc`
     	stdOverhead=`echo "sqrt((${stdAfterLoad})^2 - (${stdgBeforeLoad})^2)/(10^6)" | bc`
-    	echo "Overhead is: ${avrgOverhead}MB  (std=${stdOverhead}MB)"    
+    	echo "Storage cost is: ${avrgOverhead}MB  (std=${stdOverhead}MB)"    
 	
     done
 
