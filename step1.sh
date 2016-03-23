fe65a7afe5fcd177b0d4574f77fc3bcdc715fd5e
Fabio Coelho <fabio.a.coelho@inesctec.pt>
2016-03-23 21:20:46 +0000

    removing wrong files from repo

--- step1.sh
+++
@@ -1,8 +0,0 @@
-export PARSER_HOME=`dirname "$this"`
-
-echo 'currently parsing file: '
-for f in $PARSER_HOME/ReplayLogs/*; do
-  echo $f
-  zcat $f | java -cp  $PARSER_HOME/target/ycsb-Wiki_dumps_replay-jar-with-dependencies.jar com.yahoo.ycsb.workloads.Parser step1 $PARSER_HOME/dbs $@
-done
-echo 'Thats all Folks!'
